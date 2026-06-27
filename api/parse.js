81e6ff3e5ed45378cce78a508e0a00aa18106bc7
a282504671-afk <a282504671@gmail.com>
2026-06-27 14:55:20 +0800

    api/parse.js

---
+++ api/parse.js
@@ -0,0 +1,6 @@
+module.exports = function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  var url = req.query.url || req.query.u || 'none';
+  res.status(200).json({ ok: true, message: 'Vercel works!', yourUrl: url, method: req.method });
+};
